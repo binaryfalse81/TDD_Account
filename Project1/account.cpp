e8faae3466f838ac130e1b5aa3dc7e09d8c4223a
JJY <binaryfalse81@gmail.com>
2024-07-03 09:06:31 +0900

    tc 추가 및 3차 refactoring

--- Project1/account.cpp
+++ Project1/account.cpp
@@ -1,13 +1,18 @@
 class Account
 {
 public:
-    Account(int n) : balance { n }
+    Account(int money) : balance { money }
     {
     }
 
     int getBalance()
     {
         return balance;
+    }
+
+    void deposit(int money)
+    {
+        balance += money;
     }
 private:
     int balance;
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,15 @@
 #include "pch.h"
 #include "../Project1/account.cpp"
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, CreateAccountInit10000) {
     Account account { 10000 };
     int ret = account.getBalance();
     EXPECT_EQ(10000, ret);
+}
+
+TEST(TestCaseName, Deposit) {
+    Account account{ 10000 };
+    account.deposit(500);
+    int ret = account.getBalance();
+    EXPECT_EQ(10500, ret);
 }
